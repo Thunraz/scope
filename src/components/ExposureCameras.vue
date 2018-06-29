af221d33292f7702e15a5d7b3dc44e7bae9e5790
Thunraz <thunraz@thunraz.com>
2018-06-29 21:07:48 +0200

    Added optimal exposure time calculation

---
+++ src/components/ExposureCameras.vue
@@ -0,0 +1,71 @@
+<template lang="pug">
+form.exposure-cameras
+    .form-row
+        legend.form-label Camera
+            span(v-if="cameraList.length != 1") s
+    
+    b-table(striped, hover, outlined, :items="cameraList", :fields="cameraFields")
+        template(slot="HEAD_qe", slot-scope="data")
+            span(v-b-tooltip.hover="", title="Quantum efficiency") {{data.label}}
+
+        template(slot="readNoise", slot-scope="data")
+            span(v-if="data.value") {{data.value}}&thinsp;e&minus;
+
+        template(slot="HEAD_singleExp", slot-scope="data")
+            span(v-b-tooltip.hover="", title="Optimal exposure time for a single exposure") {{data.label}}
+
+        template(slot="singleExp", slot-scope="data")
+            span(v-if="calcSingleExp(data.item)") {{round(calcSingleExp(data.item), 0)}}&thinsp;s
+
+        template(slot="HEAD_numberOfExps", slot-scope="data")
+            span(v-b-tooltip.hover="", title="Number of exposures to match the target exposure time as close as possible") {{data.label}}
+
+        template(slot="numberOfExps", slot-scope="data")
+            span(v-if="calcSingleExp(data.item)") {{round(calcNumberOfExps(data.item), 1)}}
+
+</template>
+
+<script>
+export default {
+    name: 'ExposureCameras',
+
+    props: ['targetExposure', 'lightPollution', 'extraNoise'],
+
+    data: () => { return {
+        cameraFields: {
+            name:         { label: 'Name', sortable: true },
+            qe:           { label: 'QE', sortable: true, formatter: 'formatQE' },
+            readNoise:    { sortable: true },
+            singleExp:    { label: 't', formatter: 'formatSingleExp' },
+            numberOfExps: { label: 'n' }
+        }
+    }},
+    
+    computed: {
+        cameraList: {
+            get() { return this.$store.state.cameras; }
+        }
+    },
+
+    methods: {
+        formatQE: function(qe) {
+            return qe * 100 + '\u2009%';
+        },
+
+        round(number, digits) {
+            let factor = Math.pow(10, digits);
+            return Math.round(number * factor) / factor;
+        },
+
+        calcSingleExp(item) {
+            let extraNoiseFactor = 1 / (Math.pow(1 + this.extraNoise / 100, 2) - 1);
+            let noiseFactor = Math.pow(item.readNoise, 2) / (item.qe * this.lightPollution);
+            return extraNoiseFactor * noiseFactor;
+        },
+
+        calcNumberOfExps(item) {
+            return this.targetExposure * 60 / this.calcSingleExp(item);
+        }
+    }
+}
+</script>
--- src/components/ExposureTime.vue
+++ src/components/ExposureTime.vue
@@ -1,11 +1,61 @@
 <template lang="pug">
 form#exposure-time
-    |woot
+    p
+        |Use this tool to calculate the optimal exposure time for your camera(s).
+        |Based on #[a(href="https://forums.sharpcap.co.uk/viewtopic.php?f=35&t=456") these calculations] from the SharpCap developer.
+    // Target exposure time T
+    .form-group
+        label(for="target-exposure") Target exposure time in minutes
+        .input-group.col-sm-12.col-md-6.col-lg-4.col-xl-4
+            input#target-exposure.form-control(type="number", placeholder="Target exposure time", v-model="targetExposure")
+            .input-group-append
+                span.input-group-text min
+
+    // Light pollution background signal
+    .form-group
+        label(for="light-pollution")
+            |Light pollution background signal in photons (&gamma;) per pixel per second.
+            |5 &gamma;/pixel/s is a pretty strong background signal.
+        .input-group.col-sm-12.col-md-6.col-lg-4.col-xl-4
+            input#light-pollution.form-control(type="number", placeholder="Light pollution background signal", v-model="lightPollution")
+            .input-group-append
+                span.input-group-text &gamma; per pixel per second
+    
+    // Max allowed extra noise
+    .form-group
+        label(for="extra-noise")
+            |The amount of extra noise we are prepared to accept. 5-10% is a reasonable range.
+        .input-group.col-sm-12.col-md-6.col-lg-4.col-xl-4
+            input#extra-noise.form-control(type="number", min="1", max="100", step="1" placeholder="Allowed extra noise", v-model="extraNoise")
+            .input-group-append
+                span.input-group-text %
+
+    exposure-cameras(:targetExposure="targetExposure", :lightPollution="lightPollution", :extraNoise="extraNoise")
 </template>
 
 <script>
+import ExposureCameras from '@/components/ExposureCameras';
+
 export default {
-    name: 'ExposureTime'
+    name: 'ExposureTime',
+
+    components: {
+        ExposureCameras
+    },
+    
+    data: () => { return {
+        targetExposure: 60,
+        lightPollution: 5,
+        extraNoise: 5
+    }},
+
+    computed: {
+        cameraList: {
+            get() {
+                return this.$store.state.cameras;
+            }
+        }
+    }
+
 }
 </script>
-
